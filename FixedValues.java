825a7f6905f2f806559d4601e870ba817cb6f6b8
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-05-29 15:02:19 +0800

    Added initial java files

---
+++ FixedValues.java
@@ -0,0 +1,7 @@
+class FixedValues{
+    public final static int NO_OF_MONTHS = 12;
+    public final static int NO_OF_DAYS_IN_JMMJAOD_MONTH = 31; // JMMJAOD: January, March, May, July, August, October, December
+    public final static int NO_OF_DAYS_IN_AJSN_MONTH = 30; //AJSN: April, June, September, November
+    public final static int NO_OF_DAYS_IN_FEBRUARY = 28;
+    public final static int NO_OF_DAYS_FEBRUARY_LEAP_YEAR = 29;
+}
