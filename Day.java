b1bae3d347184d4298387dab6385a341a9be8b19
ChrisanderJerv <chrisander_yu@dlsu.edu.ph>
2025-06-10 23:20:24 +0800

    Update Day.java

--- Day.java
+++ Day.java
@@ -1,3 +1,11 @@
 public class Day {
+    private int dayNumber;
     
+    public Day (int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
 }
