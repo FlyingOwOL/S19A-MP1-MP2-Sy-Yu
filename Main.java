4cf18909e88c76eba0ba318b6201eb81fd5b9925
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-06-11 18:55:57 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Main {
+    public ArrayList<Accounts> accounts = new ArrayList<>();
+    public ArrayList<Calendar> publicCalendars = new ArrayList<>();
+    public ArrayList<Accounts> activeAccounts = new ArrayList<>();
+    public ArrayList<Accounts> deactivatedAccounts = new ArrayList<>();
     public static void main(String[] args){
             
     }
