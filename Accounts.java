d29fd9902ac2340e4aebd956dd642c685ca3217f
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-06-11 18:50:57 +0800

    Added Accounts
    Added Account attributes/getters/setters

--- Accounts.java
+++ Accounts.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+
 public class Accounts {
-    
+    private String accountName;
+    private String accountPasword;
+    private ArrayList<Calendar> privateCalendars;
+    private ArrayList<Calendar> publicCalendars;
+    private boolean isActive;
+
+    public Accounts(String accountName, String accountPassword) {
+        this.accountName = accountName;
+        this.accountPasword = accountPassword;
+        this.privateCalendars = new ArrayList<>();
+        this.publicCalendars = new ArrayList<>();
+        this.isActive = true; // Default to active
+    }
+
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountPassword() {
+        return accountPasword;
+    }
+
+    public ArrayList<Calendar> getPrivateCalendars() {
+        return privateCalendars;
+    }
+
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public boolean addCalendar(){
+        return false;
+    }
+
+    public boolean removeCalendar (Calendar calendar) {
+        return false;
+    }
 }
