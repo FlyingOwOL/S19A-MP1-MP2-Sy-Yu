aa2f7ddd3d625df03669995be82bb326095b960d
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-06-11 18:42:32 +0800

    Added Calendar class
    Finished implementation based on UML diagram

--- Calendar.java
+++ Calendar.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+
 public class Calendar {
-    
+    private String calendarName;
+    private int calendarYear;
+    private int numberOfMonths = FixedValues.NO_OF_MONTHS;
+    private boolean isPubliclyAvailable;
+    private ArrayList<Entry> calendarEntry;
+    private Month [] months = new Month[FixedValues.NO_OF_MONTHS];
+
+    public Calendar (String calendarName, boolean isPubliclyAvailable, int calendarYear){
+        this.calendarName = calendarName;
+        this.isPubliclyAvailable = isPubliclyAvailable;
+        this.calendarYear = calendarYear;
+    }
+
+
+    public String getCalendarName() {
+        return calendarName;
+    }
+
+    public int getNumberOfMonths() {
+        return numberOfMonths;
+    }
+
+    public Month getMonth (String name){
+        return null;
+    }
+
+    public boolean getAvailability() {
+        return isPubliclyAvailable;
+    }
+
+    public boolean addEntry (Entry entry) {
+        return false;
+    }
+
+    public boolean deleteEntry (Entry entry) {
+        return false;
+    }
+
+    public boolean editEntry (int entryID, Entry newEntry) {
+        return false;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args){
-        
+            
     }
 }
