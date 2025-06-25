c0466f2959de02543040367168fe6f5d9e38d4ea
ASSistant Chef of the 4th Axis Armenia Batallion <160565653+FlyingOwOL@users.noreply.github.com>
2025-06-25 21:08:52 +0800

    yezzur

--- MainMethods.java
+++ MainMethods.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class MainMethods {
-    
+    public static void displayMenu(){
+        System.out.println("Welcome to the Calendar Application!");
+        System.out.println("Please choose an option:");
+        System.out.println("1. create a new account");
+        if (Main.activeAccounts.size() > 0)
+            System.out.println("2. login to account");
+        System.out.println("0. exit the application");
+        System.out.print("Enter your choice: ");
+    }
+
+
+    public static Accounts createAccount(Scanner userInput, ArrayList<Accounts> accountsList){
+        Accounts newAccount = null;
+        boolean isValid = false;
+        userInput.nextLine(); // Consume the newline character
+        String accountName = "";
+        do{
+            System.out.println("Enter your account name: ");
+            accountName = userInput.nextLine();
+            for (Accounts account : accountsList){
+                if (account.getAccountName().equalsIgnoreCase(accountName)){
+                    System.out.println("Account name already exists. Please choose a different name.");
+                } else {
+                    isValid = true;
+                }
+            }
+        } while (!isValid);
+        System.out.println("Enter your account password:");
+        String accountPassword = userInput.nextLine();
+        newAccount = new Accounts(accountName, accountPassword);
+        return newAccount;
+    }
 }
